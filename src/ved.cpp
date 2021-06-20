b94ea85699cd34e0d61cbe087c94e3ddc8273b34
Matt Hevern <matt.hevern@gmail.com>
2021-06-20 11:06:54 +1000

    cmdline: Parse with cxxopts

--- src/ved.cpp
+++ src/ved.cpp
@@ -1,10 +1,53 @@
-#include <iostream>
+#include <string>
 #include <cxxopts.hpp>
+#include <spdlog/spdlog.h>
+
+
+cxxopts::ParseResult parse(int argc, char** argv, cxxopts::Options& options)
+{
+    try
+    {
+        auto result = options.parse(argc, argv);
+        if (!result.count("port"))
+        {
+            throw(cxxopts::option_required_exception("port"));
+        }
+        return result;
+    }
+    catch (cxxopts::OptionException& ex)
+    {
+        spdlog::error(ex.what());
+        spdlog::error(options.help());
+        exit(EXIT_FAILURE);
+    }
+}
+
 
 int main(int argc, char** argv)
 {
     cxxopts::Options options("MyProgram", "One line description of MyProgram");
-    auto result = options.parse(argc, argv);
+    options.add_options()
+        ( "d,debug", "Enable debugging")
+        ( "h,help", "Print help")
+        ( "p,port", "Serial port", cxxopts::value<std::string>())
+        ;
+
+    auto result = parse(argc, argv, options);
+
+    if (result.count("debug"))
+    {
+        // Set global log level to debug
+        spdlog::set_level(spdlog::level::debug);
+    }
+
+    if (result.count("help"))
+    {
+        spdlog::error(options.help());
+        exit(EXIT_SUCCESS);
+    }
+
+    spdlog::debug("Using port {}", result["port"].as<std::string>());
+
     return 0;
 }
 
