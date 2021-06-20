9aeb8b9f0afb266cfca6653181eed0379fe77ded
Matt Hevern <matt.hevern@gmail.com>
2021-06-20 10:23:43 +1000

    opts: Use cxxopts

--- src/ved.cpp
+++ src/ved.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <cxxopts.hpp>
 
 int main(int argc, char** argv)
 {
+    cxxopts::Options options("MyProgram", "One line description of MyProgram");
+    auto result = options.parse(argc, argv);
     return 0;
 }
 
